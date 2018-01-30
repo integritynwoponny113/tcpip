7eb4d9aa7145153dd07ce8ac02d2e6b3a984dedf
student <student@M110318>
2018-01-30 13:40:24 +0900

    modify ...

--- src/tcpip1/Client.java
+++ src/tcpip1/Client.java
@@ -1,11 +1,46 @@
 package tcpip1;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
 	public static void main(String[] args) {
-		
-		int a  = 10;
-		
-		
+		String ip = "70.12.111.130";
+		int port = 7777;
+		Socket socket = null;
+		OutputStream outs = null;
+		OutputStreamWriter outw = null;
+		try {
+			System.out.println("Start Client ... ");
+			socket = new Socket(ip, port);
+			System.out.println("Connected OK ... ");
+
+			// Send Data ...
+
+			outs = socket.getOutputStream();
+			outw = new OutputStreamWriter(outs);
+			outw.write("kslee");
+			System.out.println("Send Completed....");
+		} catch (UnknownHostException e) {
+			System.out.println("UnknownHostException...");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("IOException...");
+			e.printStackTrace();
+		} finally {
+			try {
+				outw.close();
+				outs.close();
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
 	}
 
 }
